14aec701588bad80169b2f2f60e37dcc6c765200
ThangMinhLe <minhthangle718@gmail.com>
2023-04-17 21:00:57 +0700

    update 17/04

---
+++ doAn/src/QuanlySV/diaChi.java
@@ -0,0 +1,30 @@
+public class diaChi {
+    private String district;
+    private String City;
+
+    public diaChi(){
+
+    }
+
+    public diaChi (String district, String City) {
+        this.district = district;
+        this.City = City;
+    }
+
+    public String setdistrict(String district) {
+        return this.district = district;
+    }
+
+    public String getdistrict() {
+        return district;
+    }
+
+    public String setCity(){
+        return this.City = City;
+    }
+
+    public String getCity() {
+        return City;
+    }
+
+}
---
+++ doAn/src/QuanlySV/lop.java
@@ -0,0 +1,26 @@
+package QuanlySV;
+
+public class lop {
+     private String maLop;
+     private String tenLop;
+     
+     public lop() {
+
+     }
+
+     public String setMaLop(String maLop, String tenLop) {
+        return this.maLop = maLop;
+     }
+
+     public String getMaLop() {
+        return maLop;
+     }
+
+     public String setTenLop(String tenLop) {
+        return this.tenLop = tenLop;
+     }
+
+     public String getTenLop() {
+        return tenLop;
+     }
+}
